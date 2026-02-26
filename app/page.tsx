b0ab81c8da5d8e941517577f9fe448a31c89452a
bsWiwat <bs.wiwat@gmail.com>
2026-02-26 22:50:48 +0700

    add(web): status tasks ad task model.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Status from "@/components/tasks/Status";
 import { Plus } from "lucide-react";
 
 export default function Home() {
   return (
     <>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4">
+        <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 mb-8">
           <div>
             <h1 className="text-3xl font-bold  text-violet-600">
               To-Do your tasks
@@ -17,6 +18,10 @@ export default function Home() {
             <Plus className="text-white" />
             เพิ่มงานใหม่
           </button>
+        </div>
+
+        <div>
+          <Status tasks={[]} />
         </div>
       </div>
     </>
