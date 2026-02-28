b95c043e1e7091827f93bb70f540be1ebf9a1ec4
bsWiwat <bs.wiwat@gmail.com>
2026-03-01 00:07:31 +0700

    update(model): update new task model

--- models/task.ts
+++ models/task.ts
@@ -1,10 +1,28 @@
+export const TASK_STATUS = {
+  PENDING: "101001",
+  IN_PROGRESS: "101002",
+  COMPLETED: "101003",
+} as const;
+export type TaskStatus = (typeof TASK_STATUS)[keyof typeof TASK_STATUS];
+
+export const TASK_PRIORITY = {
+  LOW: "102001",
+  MEDIUM: "102002",
+  HIGH: "102003",
+} as const;
+export type TaskPriority = (typeof TASK_PRIORITY)[keyof typeof TASK_PRIORITY];
+
 export type Task = {
   id: string;
+  user_id: string;
   title: string;
-  description?: string;
-  category: string;
-  priority: "low" | "medium" | "high";
-  status?: "pending" | "on-time" | "completed" | "overdue";
-  dueDate?: Date;
+  description: string | null;
+  status_code: TaskStatus;
+  priority_code: TaskPriority;
+  category: string | null;
+  due_date: string | null;
+  created_at: string;
+  updated_at: string;
+  deleted_at: string | null;
 };
 
